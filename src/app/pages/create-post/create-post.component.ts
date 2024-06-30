f96e22d0df3964471d408566c4e238d782880399
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-06-30 23:08:47 +0530

    feat: Add CreatePostComponent for creating new posts

--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-post',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class CreatePostComponent {
 
+  postForm!: FormGroup;
+
+  constructor (private fb: FormBuilder, private router: Router,
+    private snackBar: MatSnackBar){}
+
+
+    ngOnInit(){
+      this.postForm = this.fb.group({
+        name: [null, Validators.required],
+        content: [null, Validators.required, Validators.maxLength(10000)],
+        img: [null, Validators.required],
+        postedBy: [null, Validators.required]
+      })
+    }
 }
