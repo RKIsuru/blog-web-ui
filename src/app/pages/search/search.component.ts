9b3c9fc115685c92d492a2cf62ccb23f534cba3b
Rajapaksha R.K.I.A. <142790046+RKIsuru@users.noreply.github.com>
2024-07-05 22:19:16 +0530

    feat: implement searchByName function

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PostService } from '../../service/post.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-search',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  result: any= [];
+  name: any="";
+
+  constructor(private postService: PostService, private snackBar:MatSnackBar){}
+
+  searchByName(){
+    this.postService.searchByName(this.name).subscribe(res=>{
+      this.result = res;
+    }, error=>{
+      this.snackBar.open("Something Went Wrong!", "Ok")
+    })
+  }
 }
+
